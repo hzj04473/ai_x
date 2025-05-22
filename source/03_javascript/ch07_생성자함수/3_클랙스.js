7354821fca7765baac6cbc3515762b502c663ac5
hzj04473 <hzj04473@naver.com>
2025-05-22 15:31:01 +0900

    ch07_생성자함수#완료

---
+++ source/03_javascript/ch07_생성자함수/3_클랙스.js
@@ -0,0 +1,36 @@
+class Student {
+  constructor(name, kor, mat, eng, sci) {
+    this.name = name;
+    this.kor = kor;
+    this.mat = mat;
+    this.eng = eng;
+    this.sci = sci;
+  }
+  getSum() {
+    return this.kor + this.mat + this.eng + this.sci;
+  }
+  getAvg() {
+    return this.getSum() / 4;
+  }
+
+  toString() {
+    return `name:${this.name} 
+      kor:${this.kor} 
+      mat:${this.mat} 
+      eng:${this.eng} 
+      sci:${this.sci} 
+      sum:${this.getSum()} 
+      avg:${this.getAvg()}`;
+  }
+}
+
+let hong = new Student('홍', 99, 99, 99, 99);
+console.log(hong.toString());
+console.log(`${hong}`);
+console.log(hong);
+
+// let student = [];
+// student.push(new Student('홍', 99, 99, 99, 99));
+// student.push(new Student('김', 91, 95, 99, 99));
+// student.push(new Student('백', 99, 99, 99, 19));
+// student.forEach((data, idx) => console.log(`${idx} : ${data}\n`));
