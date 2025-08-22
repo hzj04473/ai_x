e082e76457e78b9f7f95fc5f6d8d483e9748f666
Kim Jinkyoung <hzj04473@naver.com>
2025-08-22 13:09:54 +0900

    250822_Django#3-9:회원가입시 이메일 발송

--- source/12_Django/myproject/accounts/models.py
+++ source/12_Django/myproject/accounts/models.py
@@ -1,5 +1,5 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
 
 # Create your models here.
 
@@ -13,3 +13,36 @@ class Profile(models.Model):
 
     def __str__(self):
         return "{}({}-{})".format(self.user.username, self.phone_number, self.address)
+
+
+from decouple import config
+
+# 이벤트 처리 (==post_save) : profile.save() 성공시 가입인사를 메일 전송
+from django.core.mail import send_mail
+from django.db.models.signals import post_save
+
+
+def on_send_mail(sender, **kwargs):
+    print("⭐️ on_send_mail :", kwargs)
+    if kwargs["created"]:  # TRUE : 회원가입, False : 각종 회원정보 수정
+        user = kwargs["instance"].user  # sender
+        if not user.email:  # 회원가입시 메일 입력 안됨
+            print("⭐️ 메일 주소가 없어서 메일 못 보내요")
+            return
+        # 메일 보내기
+        subject = f"{user.username}님 xxx에 가입해 주셔서 감합니다."
+        body = f"안녕하세요. {user.username}님 가입인사를 받으시면, 최고의 서비스를 드립니다. 어쩌구 저쩌구 (메일내용)"
+        bodyHtml = f"<h1>안녕하세요. {user.username}님 가입인사를 받으시면, 최고의 서비스를 드립니다. 어쩌구 저쩌구 (메일내용)</h1>"
+
+        send_mail(
+            subject=subject,
+            message=body,
+            from_email=config("EMAIL_HOST_USER"),
+            recipient_list=[user.email],
+            html_message=bodyHtml,
+            fail_silently=False,  # 메일 전송이 안 되었을떄, 아무일도 하지 않음
+        )
+
+
+# on_send_mail 함수와 post_save로 연결
+post_save.connect(on_send_mail, sender=Profile)
--- source/12_Django/myproject/myproject/settings.py
+++ source/12_Django/myproject/myproject/settings.py
@@ -16,8 +16,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 5. timezone 등록
 """
 
-from pathlib import Path
 import os
+from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -153,3 +153,13 @@ MEDIA_URL = "/media/"
 
 # 업로드한 파일이 저장될 폴더 -> 이미지 파일 지원 라이브러리 pip install pillow
 MEDIA_ROOT = os.path.join(BASE_DIR, "_media")
+
+
+# EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 465
+EMAIL_HOST_USER = config("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
+EMAIL_USE_SSL = True
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
